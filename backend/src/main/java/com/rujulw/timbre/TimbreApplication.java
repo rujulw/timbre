646907b9640e0aebb0fd310acbd75fb1b8dff76b
rujulw <wrujul26@gmail.com>
2026-03-05 15:00:39 -0500

    [impl] add backend spring boot bootstrap with maven wrapper

---
+++ backend/src/main/java/com/rujulw/timbre/TimbreApplication.java
@@ -0,0 +1,12 @@
+package com.rujulw.timbre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimbreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimbreApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/rujulw/timbre/TimbreApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rujulw.timbre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimbreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
